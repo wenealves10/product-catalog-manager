dde7dcad969d62da343bcc8c69b00ad8b4091820
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-06-15 10:29:06 -0300

    Product Service in the project 👍

---
+++ src/products/products.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product, ProductDocument } from './entities/product.entity';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+  ) {}
+
+  create(createProductDto: CreateProductDto) {
+    const user = new this.productModel(createProductDto);
+    return user.save();
+  }
+
+  findAll() {
+    return this.productModel.find();
+  }
+
+  findOne(id: string) {
+    return this.productModel.findById(id);
+  }
+
+  update(id: string, updateProductDto: UpdateProductDto) {
+    return this.productModel.findByIdAndUpdate(
+      {
+        _id: id,
+      },
+      { $set: updateProductDto },
+      {
+        new: true,
+      },
+    );
+  }
+
+  remove(id: string) {
+    return this.productModel
+      .deleteOne({
+        _id: id,
+      })
+      .exec();
+  }
+}
